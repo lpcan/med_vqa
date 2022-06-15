31c4451ace324e62cf4dc2da850ed4d710f02848
lpcan <lcanepa3@gmail.com>
2022-06-15 10:12:24 +1000

    changed dataset path

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 import torchvision.transforms as transforms
 
 train_val_split = 0.8
-data = "../Datasets/VQA-RAD/"
+data = "Datasets/VQA-RAD/"
 wv_path = "bio_embedding_extrinsic"
 batch_size = 64
 epochs = 100
