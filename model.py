1e79ebe0aa0a19de709922a01d1ddc9da82d0845
Louisa Canepa <lcanepa3@gmail.com>
2022-08-14 09:50:14 +1000

    implemented what san is actually supposed to be

--- model.py
+++ model.py
@@ -8,15 +8,19 @@ class ImgEncoder(nn.Module):
     # VGG-16
     def __init__(self, out_size):
         super(ImgEncoder, self).__init__()
-        self.model = models.vgg16(pretrained=True) # load model
+        self.model = models.vgg16(pretrained=True).features # load model and pull out the part we want
+        self.linear = nn.Linear(512, out_size) # Reshape to match q_feat size
+
         #in_feat = self.model.classifier[6].in_features
         #self.model.classifier[6] = nn.Linear(in_feat, out_size) # replace the output layer to give the size we want
-        del self.model.classifier # Remove the dense layers
-        self.model.avgpool = nn.AdaptiveAvgPool2d(output_size=1) # Global Average Pooling [1x1x]
 
     def forward(self, input):
-        out = self.model(input) # we are finetuning the entire model, so no need to freeze parameters
-        # normalisation???
+        out = self.model(input) #  shape [batch_size, 512, 8, 8]
+        out = out.permute(0, 2, 3, 1) # reshape to [batch_size, 8, 8, 512]
+        out = out.view(-1, 64, 512) # reshape to [batch_size, 64, 512]
+        #out = torch.tanh(self.linear(out)) # apply linear layer
+        out = self.linear(out) # apply linear layer
+
         return out
 
 class QEncoder(PreTrainedModel):
@@ -46,7 +50,7 @@ class SAN(nn.Module):
         for i in range(num_layers):
             list_img_layer.append(nn.Linear(feat_size, hidden_size))
             list_q_layer.append(nn.Linear(feat_size, hidden_size))
-            list_attn.append(nn.Linear(hidden_size, feat_size))
+            list_attn.append(nn.Linear(hidden_size, 1))
         
         # Convert the lists into nn.ModuleLists so that Pytorch can see them
         self.img_layer = nn.ModuleList(list_img_layer)
@@ -61,16 +65,16 @@ class SAN(nn.Module):
         # Repeat for the desired number of layers
         for i in range(self.num_layers):
             # Pass image and question vectors through layers
-            hidden_vec = torch.tanh(self.img_layer[i](img_vector) + self.q_layer[i](refined_query))
+            hidden_vec = torch.tanh(self.img_layer[i](img_vector) + self.q_layer[i](refined_query).unsqueeze(1)) # [batch_size, 64, hidden_size]
 
             # Get attention distribution
-            attn_dist = F.softmax(self.attn[i](hidden_vec), dim=1)
+            attn_dist = F.softmax(self.attn[i](hidden_vec), dim=1) # [batch_size, 64, 1]
 
-            # Get the weighted image vector
-            weighted_vec = attn_dist * img_vector
+            # Get the weighted image vector and aggregate
+            weighted_vec = torch.sum(attn_dist * img_vector, dim=1) # [batch_size, feat_size]
 
             # Get the refined query vector
-            refined_query = weighted_vec + refined_query
+            refined_query = weighted_vec + refined_query # [batch_size, feat_size]
 
         return refined_query
 
@@ -81,10 +85,12 @@ class AnsGenerator(nn.Module):
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(drop)
         self.lin1 = nn.Linear(embed_size,  pos_ans)
+        #self.gap = nn.AdaptiveAvgPool2d(output_size=1) # Global Average Pooling
         self.lin2 = nn.Linear(pos_ans, pos_ans)
     def forward(self, input):
         x = self.dropout(input)
         x = self.lin1(x)
+        #x = self.gap(x)
         x = self.relu(x)
         x = self.lin2(x)
         return x
@@ -100,7 +106,7 @@ class VQAModel(nn.Module):
         self.classifier = AnsGenerator(feat_size, out_size, drop=dropout)
 
     def forward(self, v, q):
-        img_feat = self.img_encoder(v) # [batch_size, feat_size]
+        img_feat = self.img_encoder(v) # [batch_size, 64, feat_size]
         q_feat = self.q_encoder(q) # [batch_size, feat_size]
         fused_feat = self.attention(img_feat, q_feat) # [batch_size, feat_size]
 
--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def train():
     # Initialise model, loss function, and optimiser
     print("Initialising model...")
     num_answers = len(ans_translator.answer_list)
-    model = VQAModel(feat_size=512, out_size = num_answers, num_layers = params.num_attn_layers, dropout=0.5)
+    model = VQAModel(feat_size=256, out_size = num_answers, num_layers = params.num_attn_layers, dropout=0.5)
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimiser = optim.Adam(model.parameters(), lr=params.lr)
